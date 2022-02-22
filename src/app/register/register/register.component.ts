8b628bbc06f381b239f098205cd100be8e1072c5
jotas <pepejavichuy@gmail.com>
2022-02-21 23:05:52 -0700

    agregando diseño con TS

--- src/app/register/register/register.component.ts
+++ src/app/register/register/register.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  titulo = 'Registro';
+  estado = 'esperando';
+  nombre: string = '';
+  correo: string = '';
+  contra: string = '';
 
+  registrar() {
+    if (
+      this.nombre == null ||
+      this.nombre == undefined ||
+      this.nombre.trim() == ''
+    ) {
+      this.estado = 'nombre vacio';
+      return;
+    }
+    if (this.correo == null || this.correo == undefined) {
+      this.estado = 'correo vacio';
+      return;
+    }
 
+    if (
+      this.contra == null ||
+      this.contra == undefined ||
+      this.contra.trim() == ''
+    ) {
+      this.estado = 'contraseña vacia';
+      return;
+    }
+    this.estado = 'registrado';
+  }
 }
